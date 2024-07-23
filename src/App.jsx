8d7cfd4a9ee72e7dc58abac0eefb0fa6f67e6c71
John Mark Delima <delimajohnmark14@gmail.com>
2024-07-23 19:11:47 +0800

    Add collaborations section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import ButtonGradient from './assets/svg/ButtonGradient';
 import Benefits from "./components/Benefits";
+import Colaborations from "./components/Colaborations";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 
@@ -10,6 +11,7 @@ const App = () => {
         <Header />
         <Hero />
         <Benefits />
+        <Colaborations />
       </div>
       <ButtonGradient />
     </>
